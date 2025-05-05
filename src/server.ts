f70d9dd90e667d10df8983b0b36eec034c54f768
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-05-05 00:41:48 -0300

    feat: add server setup with Fastify, CORS, Swagger, and Zod
    This commit introduces a new server setup using Fastify, enabling CORS for cross-origin requests, integrating Swagger for API documentation, and utilizing Zod for schema validation. The server listens on the port specified in the environment configuration.

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import { env } from "./env";
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import {
+  type ZodTypeProvider,
+  validatorCompiler,
+  serializerCompiler,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+
+const server = fastify({ logger: true }).withTypeProvider<ZodTypeProvider>();
+
+// temporary solution
+server.register(fastifyCors, {
+  origin: true,
+});
+
+server.setSerializerCompiler(serializerCompiler);
+server.setValidatorCompiler(validatorCompiler);
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "short url api",
+      description: "short url api",
+      version: "0.1.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+server.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+try {
+  server.listen({ port: env.PORT });
+  console.log("server started");
+} catch (error) {
+  console.log(error);
+}
