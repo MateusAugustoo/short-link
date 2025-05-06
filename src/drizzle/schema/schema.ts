5e6cf694ebf0198d9829f83f52a8893185f959a2
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-05-06 01:05:48 -0300

    refactor(schema): remove unused schema file
    The schema.ts file was deleted as it is no longer needed in the project. This cleanup helps reduce unnecessary code and improves maintainability.

--- src/drizzle/schema/schema.ts
+++
@@ -1,9 +0,0 @@
-import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core'
-
-export const link = sqliteTable('link', {
-  id: integer('id').primaryKey(),
-  originalUrl: text('original_url').notNull(),
-  shortId: text('short_id').notNull(),
-  createAt: integer('create_at', { mode: 'timestamp_ms' }).notNull(),
-  expiresAt: integer('expires_at', { mode: 'timestamp_ms' })
-})
