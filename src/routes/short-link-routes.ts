d5ae68a3457f9f53032e8ffbfd1a2ae3c8ce32a8
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-05-25 00:45:41 -0300

    feat(short-link-routes): add endpoint to redirect and track visits
    Add a new GET endpoint `/shortId` to handle redirection to the original URL and track the number of visits using `addQuantVisitant` function. This enhances the functionality by providing analytics on link usage.

--- src/routes/short-link-routes.ts
+++ src/routes/short-link-routes.ts
@@ -3,6 +3,7 @@ import { db } from "../drizzle/client";
 import { insertLink } from "../functions/insert-link";
 import { generateShortLinkId } from "../functions/generate-short";
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { addQuantVisitant } from '../functions/add-quant-visitant'
 
 export const shortLinkRoutes: FastifyPluginAsyncZod = async (app) => {
   app.post(
@@ -55,7 +56,41 @@ export const shortLinkRoutes: FastifyPluginAsyncZod = async (app) => {
         return reply.code(201).send({
           shortUrl,
         });
-      } catch (error) {}
+      } catch (error) { }
     }
   );
+
+  app.get('/:shortId', {
+    schema: {
+      tag: ['Short'],
+      params: z.object({
+        shortId: z.string()
+      }),
+      response: {
+        404: z.object({})
+      }
+    }
+  }, async (request, reply) => {
+    const { shortId } = request.params;
+
+    const link = await db.query.links.findFirst({
+      where: (links, { eq }) => eq(links.shortId, shortId),
+    });
+
+    if (!link) {
+      return reply.code(404).send({
+        message: 'Link not found'
+      })
+    }
+
+    try {
+      await addQuantVisitant({ id: link.id })
+
+      return reply.code(302).redirect(link.originalUrl)
+    } catch (error) {
+      return reply.code(500).send({
+        message: 'Internal server error'
+      })
+    }
+  })
 };
